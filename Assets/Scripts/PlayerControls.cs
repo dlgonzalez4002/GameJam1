a4e561d8a4e7ada4d322ea800a026556356a7f12
SomeProgrammer <gjw817@icloud.com>
2026-02-07 09:13:47 -0700

    Player Controls
    Added script for player controls: movement, jump, and even a dash (although sometimes it is a little jittery). Also added a new tag "Ground" for reseting the jump (and dash if the player is in the air).

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,120 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System;
+
+public class PlayerControls : MonoBehaviour
+{
+    [SerializeField] private float jumpForce;
+    [SerializeField] private int maxJumps;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float dashForce;
+    private bool canJump = true;
+    private int numJumps = 0;
+    private bool canDash = true;
+    private bool isDashing = false;
+    private bool inAir = false;
+    private float playerSpeed = 0f;
+    private Rigidbody rb;
+    private Vector2 moveInput;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        Debug.Log(rb.linearVelocity.x/playerSpeed);
+        // Only cares about the velocities in the x and z direction
+        playerSpeed = (float) Math.Sqrt(Math.Pow(rb.linearVelocity.x, 2) + Math.Pow(rb.linearVelocity.z, 2));
+
+        // The player is dashing when they are moving faster than their movespeed
+        if (playerSpeed > moveSpeed)
+        {
+            isDashing = true;
+        }
+        
+        // !canDash is asking if the player is dashing, false meaning they are
+        if (isDashing)
+        {
+            rb.AddForce(-Mathf.Sign(rb.linearVelocity.x)*5/2, 0, -Mathf.Sign(rb.linearVelocity.z)*5/2);
+            if (playerSpeed <= moveSpeed)
+            {
+                isDashing = false;
+
+                if (!inAir)
+                {
+                    canDash = true;
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+        
+
+        // Moves the player based on their input
+        rb.AddForce(moveInput.x * moveSpeed, 0, moveInput.y * moveSpeed);
+
+        if (playerSpeed > 0)
+        {
+            if (moveInput.x == 0 && moveInput.y == 0 && inAir)
+            {
+                return;
+            }
+
+            rb.AddForce(-rb.linearVelocity.x, 0, -rb.linearVelocity.z);
+        }
+    }
+
+    void OnJump()
+    {
+        // numJumps < maxJumps allows for multiple jumps should maxJumps > 1
+        if (canJump && numJumps < maxJumps)
+        {
+            rb.AddForce(0, jumpForce, 0, ForceMode.Impulse);
+            numJumps++;
+        } 
+        else
+        {
+            canJump = false;
+        }
+    }
+
+    void OnDash()
+    {
+        if (canDash)
+        {
+            canDash = false;
+            rb.AddForce(rb.linearVelocity.x/playerSpeed * dashForce, 0, rb.linearVelocity.z/playerSpeed * dashForce, ForceMode.Impulse);
+        }
+    }
+
+    void OnMove(InputValue input)
+    {   
+        moveInput = input.Get<Vector2>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // If the player hits the ground, resets their jump cycle
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            inAir = false;
+
+            canJump = true;
+            numJumps = 0;
+
+            canDash = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        // When the player leaves the ground they are in the air
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            inAir = true;
+        }
+    }
+}
